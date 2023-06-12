65b773c0e68b4dbd77d8d513f3d565b7122a0116
Matheusg2002 <126262144+Matheusg2002@users.noreply.github.com>
2023-06-12 09:11:16 -0300

    Mapa da Floresta e assets do mapa

---
+++ cliente/assets/A2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="A2" tilewidth="16" tileheight="16" tilecount="756" columns="28">
+ <image source="A2.png" width="448" height="432"/>
+</tileset>
